4b4b5ad9b5ed23a0d73164c4c53fa51a78a60bee
Ronaldo Costa Miranda <ronaldocosta.07101986@gmail.com>
2025-05-08 21:49:57 -0300

    Criação da classe de estilização do app.
    Criação da classe de estilização do app.

--- routerapp/lib/main.dart
+++ routerapp/lib/main.dart
@@ -10,9 +10,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RouterApp',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: appTheme, //ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
       home: RotaScreen(),
     );
   }
 }
+
+ThemeData appTheme = ThemeData(
+  textTheme: const TextTheme(
+    headlineLarge: TextStyle(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.indigo,
+    ),
+    bodyMedium: TextStyle(fontSize: 18.0, color: Colors.black87),
+  ),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.indigo,
+    titleTextStyle: TextStyle(color: Colors.white, fontSize: 20.0),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.blueGrey,
+      foregroundColor: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      textStyle: const TextStyle(fontSize: 16.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+    ),
+  ),
+  colorScheme: ColorScheme.fromSwatch(
+    primarySwatch: Colors.blueGrey,
+  ).copyWith(secondary: Colors.pinkAccent),
+);
